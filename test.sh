02bcc95257564bd3af82f7a991dc853fc8deaa8a
Chuong Le <chuong.le@skymavis.com>
2023-03-07 21:23:25 +0700

    chore: test

--- test.sh
+++ test.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+set -eo pipefail
 
-echo ${1}
-echo ${GITHUB_RUN_NUMBER}
+create_env_config() {
+  if expr "${1}" : "dev" > /dev/null; then
+      export build_name='Homeland'
+  
+      export build_arguments=(
+          -architect 'Mono'
+          -has_log 'true'
+          -display_name 'Homeland'
+          -mac_architect 2
+      )
+  elif expr "${1}" : "prod" > /dev/null; then
+      export build_name='Axie Infinity - Homeland'
+  
+      export build_arguments=(
+          -architect 'IL2CPP'
+          -define_symbols 'RELEASE'
+          -display_name 'Axie Infinity - Homeland'
+          -mac_architect 2
+      )
+  fi
+}
+
+export BUILD_NAME="${BUILD_NAME_OVERWRITE:-homeland-${1}-${2}-${GITHUB_RUN_NUMBER}-macos}"
+create_env_config
+echo $build_name
+echo ${build_arguments[@]}
